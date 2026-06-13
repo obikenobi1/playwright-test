a95a75d1caa46f0130b997b18cc717a283ec59e9
ariel <ariel.blackdragon@gmail.com>
2026-06-13 19:36:19 +0700

    add simple test. create playwrite driver,browser, and page. add assert

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,33 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void shouldShowThePageTitle(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void shouldSearchByKeyword(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+    }
+}
