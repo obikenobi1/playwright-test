c089325ac84a35c947c3a1d28e9a7e3500e89f3e
ariel <ariel.blackdragon@gmail.com>
2026-06-13 20:05:55 +0700

    refactor using @useplaywright

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,33 +1,33 @@
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class SimpleTest {
-    @Test
-    void shouldShowThePageTitle(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
 
+    @Test
+    void shouldShowThePageTitle(Page page){
         page.navigate("https://practicesoftwaretesting.com/");
         String title = page.title();
 
         Assertions.assertTrue(title.contains("Practice Software Testing"));
 
-        browser.close();
-        playwright.close();
+
     }
 
     @Test
-    void shouldSearchByKeyword(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-
+    void shouldSearchByKeyword(Page page){
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
+        int matching = page.locator(".card").count();
+
+        Assertions.assertTrue(matching > 0);
+
     }
 }
