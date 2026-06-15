b38c881b7e977cc4a10f93814f83b9ec483bbdc7
ariel <ariel.blackdragon@gmail.com>
2026-06-15 21:37:18 +0700

    using filter and test id for checking out of stock items. assert when moving to new url

--- src/test/java/SimpleTestCustomize.java
+++ src/test/java/SimpleTestCustomize.java
@@ -1,11 +1,13 @@
 import com.microsoft.playwright.*;
-import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SimpleTestCustomize {
 
@@ -25,6 +27,8 @@ public class SimpleTestCustomize {
                         .setArgs(Arrays.asList("--disable-extensions"))
         );
         browserContext = browser.newContext();
+        playwright.selectors().setTestIdAttribute("data-test");
+
     }
 
     @BeforeEach
@@ -40,6 +44,7 @@ public class SimpleTestCustomize {
 
     @Test
     void shouldShowThePageTitle(){
+
         page.navigate("https://practicesoftwaretesting.com/");
         String title = page.title();
 
@@ -58,4 +63,35 @@ public class SimpleTestCustomize {
         Assertions.assertTrue(matching > 0);
 
     }
+
+    @Test
+    void checkOutOfStock(){
+        page.navigate("https://practicesoftwaretesting.com/");
+        //auto locator for menubar
+        Locator menubar = page.getByRole(AriaRole.MENUBAR, new Page.GetByRoleOptions().setName("Main Menu"));
+        Locator categoriesBtn = menubar.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Categories"));
+        //since the URL is under menubar element, not on categories element. I use menubar Locator for get by role and it works.
+        Locator handToolsLink = menubar.getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("Hand Tools"));
+
+        categoriesBtn.click();
+        handToolsLink.click();
+        assertThat(page).hasURL("https://practicesoftwaretesting.com/category/hand-tools");
+
+        //using wait because the list need to load first
+        page.waitForSelector("a.card");
+
+        //filter usage
+        List<String> allProducts= page.getByTestId("product-name").filter(new Locator.FilterOptions().setHasNotText("Drill")).allTextContents();
+        List<String> outOfStockItems = page.locator("a.card")
+                .filter(new Locator.FilterOptions().setHasText("Out of stock"))
+                .getByTestId("product-name")
+                .allTextContents()
+                .stream()
+                .map(String::trim)
+                .toList();
+        //need trim because have inconsistent space
+        Assertions.assertTrue(outOfStockItems.contains("Long Nose Pliers"));
+
+    }
+
 }
